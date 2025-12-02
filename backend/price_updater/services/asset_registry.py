7fec276ee38f1f77c71cd74f9b9d1df131cf1534
wisetree911 <gariksharik224@gmail.com>
2025-12-02 21:22:34 +0500

    using asset_price repository instead of raw orm in backend/price_updater/services/asset_registry.py

--- backend/price_updater/services/asset_registry.py
+++ backend/price_updater/services/asset_registry.py
@@ -1,19 +1,18 @@
 from sqlalchemy import select
 from loguru import logger
-from shared.models.asset import Asset
-
+from shared.repositories.asset import AssetRepository
 class AssetRegistry:
     def __init__(self):
         self.assets: dict[int, str] = {}
 
     async def load(self, session):
         logger.info("**** загрузка список активов из БД ****")
-        result = await session.execute(select(Asset))
-        rows = result.scalars().all()
+        repo = AssetRepository(session=session)
+        rows = await repo.get_all()
         self.assets = {row.id: row.ticker for row in rows}
 
-    def get_all(self):
+    def get_all(self) -> dict:
         return self.assets
 
-    def get_ticker(self, asset_id: int):
+    def get_ticker(self, asset_id: int) -> str:
         return self.assets.get(asset_id)
