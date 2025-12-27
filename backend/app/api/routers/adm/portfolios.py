529eddbb5834290ff31f2fe66fb441cf42199e0d
wisetree911 <gariksharik224@gmail.com>
2025-12-28 01:12:01 +0500

    refactor: передвинул все текущие ручки в папку adm - ручки администратора

--- backend/app/api/routers/adm/portfolios.py
+++ backend/app/api/routers/adm/portfolios.py
@@ -4,30 +4,34 @@ from app.services.portfolios import PortfolioService
 from app.api.deps import get_porfolio_service
 router = APIRouter(prefix="/portfolios", tags=["Portfolios"])
 
-@router.get("/{portfolio_id}")
+@router.get("/admin/{portfolio_id}")
 async def get_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.get_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
 
-@router.get("/")
+@router.get("/admin")
 async def get_all(service: PortfolioService=Depends(get_porfolio_service)) -> list[PortfolioResponse]:
     return await service.get_all_portfolios()
 
-@router.post("/")
+@router.post("/admin")
 async def create(payload: PortfolioCreate, service: PortfolioService = Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.create_portfolio(payload)
 
-@router.delete("/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/admin/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)):
     await service.delete_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
     return
 
-@router.patch("/{portfolio_id}")
+@router.patch("/admin/{portfolio_id}")
 async def update_portfolio(portfolio_id: int, payload: PortfolioUpdate, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.update(portfolio_id=portfolio_id, payload=payload)
 
-@router.get("/user/{user_id}") # add pydantic
+@router.get("/admin/user/{user_id}") # add pydantic
 async def get_user_portfolios(user_id: int, service: PortfolioService=Depends(get_porfolio_service)):
     return await service.get_user_portfolios(user_id=user_id)
 
+from app.api.deps import get_current_user
 
+@router.get("/")
+async def get_all_portfolios(current_user=Depends(get_current_user), service: PortfolioService=Depends(get_porfolio_service)) -> list[PortfolioResponse]:
+    return await service.get_user_portfolios(user_id=current_user.id)
 
