7e5939dc00c93269b6d7b306eaa5ce22d3fa15b6
wisetree911 <gariksharik224@gmail.com>
2025-12-28 02:59:30 +0500

    dev: переименовал пидантик с припиской Adm для админ ручек

--- backend/app/api/routers/adm/portfolios.py
+++ backend/app/api/routers/adm/portfolios.py
@@ -1,30 +1,30 @@
 from fastapi import APIRouter, status, Depends
-from app.schemas.portfolio import PortfolioCreate, PortfolioResponse, PortfolioUpdate
+from app.schemas.portfolio import PortfolioCreateAdm, PortfolioResponse, PortfolioUpdateAdm
 from app.services.portfolios import PortfolioService
 from app.api.deps import get_porfolio_service
 router = APIRouter(prefix="/portfolios", tags=["Portfolios"])
 
-@router.get("/admin/{portfolio_id}")
+@router.get("/{portfolio_id}")
 async def get_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.get_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
 
-@router.get("/admin")
+@router.get("/")
 async def get_all(service: PortfolioService=Depends(get_porfolio_service)) -> list[PortfolioResponse]:
     return await service.get_all_portfolios()
 
-@router.post("/admin")
-async def create(payload: PortfolioCreate, service: PortfolioService = Depends(get_porfolio_service)) -> PortfolioResponse:
+@router.post("/")
+async def create(payload: PortfolioCreateAdm, service: PortfolioService = Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.create_portfolio(payload)
 
-@router.delete("/admin/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{portfolio_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_by_id(portfolio_id: int, service: PortfolioService=Depends(get_porfolio_service)):
     await service.delete_portfolio_by_portfolio_id(portfolio_id=portfolio_id)
     return
 
-@router.patch("/admin/{portfolio_id}")
-async def update_portfolio(portfolio_id: int, payload: PortfolioUpdate, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponse:
+@router.patch("/{portfolio_id}")
+async def update_portfolio(portfolio_id: int, payload: PortfolioUpdateAdm, service: PortfolioService=Depends(get_porfolio_service)) -> PortfolioResponse:
     return await service.update(portfolio_id=portfolio_id, payload=payload)
 
-@router.get("/admin/user/{user_id}") # add pydantic
+@router.get("/user/{user_id}") # add pydantic
 async def get_user_portfolios(user_id: int, service: PortfolioService=Depends(get_porfolio_service)):
     return await service.get_user_portfolios(user_id=user_id)
