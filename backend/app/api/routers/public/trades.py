4f4f3ce60aa45c2550a0042860473e153537e0b8
wisetree911 <gariksharik224@gmail.com>
2025-12-29 20:26:57 +0500

    fix: был конфликт ручек с одинаковыми путями

--- backend/app/api/routers/public/trades.py
+++ backend/app/api/routers/public/trades.py
@@ -9,7 +9,7 @@ router = APIRouter(prefix="/trades", tags=["Trades"])
 async def get_trade_for_user(trade_id: int, current_user=Depends(get_current_user), service: TradeService=Depends(get_trade_service)) -> TradeResponse:
     return await service.get_trade_by_id_for_user(trade_id=trade_id, user_id=current_user.id)
 
-@router.get("/{portfolio_id}")
+@router.get("/portfolio/{portfolio_id}")
 async def get_portfolio_trades_for_user(portfolio_id: int, current_user=Depends(get_current_user), service: TradeService=Depends(get_trade_service)) -> list[TradeResponse]:
     return await service.get_trades_portfolio_for_user(portfolio_id=portfolio_id, user_id=current_user.id)
     
