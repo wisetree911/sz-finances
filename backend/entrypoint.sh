d15f54820732db653becc843d16abd7613de532c
wisetree911 <gariksharik224@gmail.com>
2026-01-08 21:42:48 +0500

    infrastructure: обновил докерфайл теперь не копирует лишнего и стал легче, обновил энтрипоинт, теперь не флудит ревизиями алембика в волюм при каждом перезапуске

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-alembic revision --autogenerate
 alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers
