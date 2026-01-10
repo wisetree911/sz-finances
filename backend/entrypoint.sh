1f81f183e522b2a92b97b69d233ff727a0e03afa
wisetree911 <gariksharik224@gmail.com>
2026-01-10 15:23:30 +0500

    infrastructure: обновил ентрипоинт бинрник

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
+
+echo "waiting for Postgres at ${POSTGRES_HOST}:${POSTGRES_PORT}"
+until pg_isready -h "${POSTGRES_HOST}" -p "${POSTGRES_PORT}" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}"; do
+  sleep 1
+done
 
 alembic upgrade head
+
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --proxy-headers
