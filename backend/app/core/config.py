94e0759827fcdebb56f32cb643edce93e2e32fb1
wisetree911 <gariksharik224@gmail.com>
2026-01-11 18:31:57 +0500

    infrastructure: переменную алгоритма шифрования в енв, поменял в композе и в конфиге

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
 
     SECRET_KEY: str
-    ALGORITHM: str = "HS256"
+    ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
     REDIS_URL: str 
