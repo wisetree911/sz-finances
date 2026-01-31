ebfd57ad048622a3541097ac6b603447db2b9bc1
wisetree911 <gariksharik224@gmail.com>
2026-01-31 20:47:03 +0500

    feat+migr: добавил поле  роль в модель юзера, мигрировал

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from app.infrastructure.db.database import Base
 from sqlalchemy import DateTime, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
@@ -11,5 +10,6 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(Text, nullable=False)
     email: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
+    role: Mapped[str] = mapped_column(Text, nullable=False, server_default='user')
     hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
