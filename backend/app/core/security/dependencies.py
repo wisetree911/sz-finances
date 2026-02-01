5b96d18f5ab50c931ff99620dfd4b1a36bf92a4f
wisetree911 <gariksharik224@gmail.com>
2026-02-01 21:57:39 +0500

    refactor: ruff

--- backend/app/core/security/dependencies.py
+++ backend/app/core/security/dependencies.py
@@ -10,7 +10,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/auth/login')
 
 async def get_current_user(
     token: str = Depends(oauth2_scheme),
-    service: UserService = Depends(get_user_service), # noqa: B008
+    service: UserService = Depends(get_user_service),  # noqa: B008
 ):
     try:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
